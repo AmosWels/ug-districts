203411cd3c6156d9a91d0c3e7a8cc84cafc5179a
Amos Welike <amoswelike@gmail.com>
2019-11-21 13:16:24 +0300

    add gitignore and function to search

---
+++ index.js
@@ -0,0 +1,12 @@
+const districts = require('./data.json');
+const myArray = districts.data;
+
+getDistrict = (nameKey) => {
+    for (var i=0; i < myArray.length; i++) {
+            if (myArray[i].asciiname = nameKey) {
+                return myArray[i];
+            } else{
+                return `No district with name ${nameKey} found`
+            }
+    }
+}
