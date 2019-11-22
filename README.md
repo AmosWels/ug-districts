68fc81aa0a56cbd4f6c44333162d9adf1939fe13
Amos Welike <amoswelike@gmail.com>
2019-11-22 17:23:42 +0300

    modify readme

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # ug-districts
-An NPM package that show districts and their population size in Uganda. 
+An NPM package that show districts and their respective population size and location coordinates in Uganda. 
+# Usage
+Install ug_districts using `yarn add ug-districts-sizes` 
+### or 
+`npm install ug-districts-sizes`
+
+## Import the getDistrict function from ug-district-sizes package as shown below;
+
+`const {getDistrict, getDistricts} = require('ug-districts-sizes');`
+
+### or
+`import { getDistrict, getDistricts} from 'ug-districts-sizes';`
+
+### Use the package using the sample below.
+#### getDistrict Usage 
+
+`const {getDistrict, getDistricts} = require('ug-districts-sizes')`
+
+`const district = () => {` 
+    `console.log(getDistrict('Mbale').location);`
+`}`
+#### getDistricts Usage
+`const {getDistrict, getDistricts} = require('ug-districts-sizes')`
+
+`const getDistricts = () => {`
+        `getDistricts.map((response)=>`
+    `console.log(response.name))`
+`}`
+
+
+
+
 
--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const districts = require('./data.json');
 const myArray = districts.data;
 
-getDistrict = (nameKey) => {
+/** @function
+ * @name getDistrict
+ *
+ * @param {string} district
+ *
+ * */
+getDistrict = (district) => {
     for (var i=0; i < myArray.length; i++) {
-            if (myArray[i].asciiname = nameKey) {
+            if (myArray[i].asciiname = district) {
                 return myArray[i];
             } else{
-                return `No district with name ${nameKey} found`
+                return `No district with name ${district} found`
             }
     }
 }
 
+/** @function
+ * @name getDistricts
+ *
+ * */
+getDistricts =()=>{
+    var myDistricts = new Object();
+    var dataSet = []
+    myArray.forEach(element => {
+        myDistricts.name = element.asciiname
+        myDistricts.location = element.location
+        myDistricts.population = element.population
+        dataSet.push(myDistricts) 
+    });
+}
+
 module.exports = {
-    getDistrict
+    getDistrict,
+    getDistricts
 }
